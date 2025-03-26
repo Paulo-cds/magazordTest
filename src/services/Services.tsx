f09992fc6f83a1009632ccedb780149eae3dbea4
Paulo <paulo.pdomingues@hotmail.com>
2025-03-26 19:03:27 -0300

    Implementação de visualização dos dados pessoais

--- src/services/Services.tsx
+++ src/services/Services.tsx
@@ -2,8 +2,6 @@ import axios from "axios";
 
 const API_URL = "https://api.github.com/users";
 
-const token = import.meta.env.TOKEN_GITHUB
-
 export const handleGetUserDataService = async (name: string) => {
     try {
         const response = await axios.get(`${API_URL}/${name}`)
